9e49acff37a97ee1e73c3ec672a6e284b390ee0f
Arvin <55039403+SurfingPanda@users.noreply.github.com>
2022-09-08 14:00:32 +0800

    Changed color of navbar and added logout function Admin Dashboarad

--- admin/includes/navbar-top.php
+++ admin/includes/navbar-top.php
@@ -1,4 +1,4 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
     <!-- Navbar Brand-->
     <a class="navbar-brand ps-3" href="index.php">Dentrix</a>
     <!-- Sidebar Toggle-->
@@ -16,11 +16,8 @@
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#!">Settings</a></li>
-                <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                <li><hr class="dropdown-divider" /></li>
                 <li>
-                    <form action="allcode.php" method="POST">
+                    <form action="../allcode.php" method="POST">
                         <button type="submit" name="logout-btn" class="dropdown-item">Logout</button>
                     </form>
                 </li>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,6 @@
-<nav class="navbar navbar-expand-lg bg-light">
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow">
   <div class="container">
     <a class="navbar-brand" href="#">Dentrix</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,10 +10,7 @@
 
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
 
         <?php if(isset($_SESSION['auth-user'])) : ?>
