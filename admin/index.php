ce6e58ff9b733ef2dab9771465baa716be578c09
Arvin <55039403+SurfingPanda@users.noreply.github.com>
2022-10-09 11:39:42 +0800

    Fixed card function

--- admin/index.php
+++ admin/index.php
@@ -11,38 +11,37 @@
     <div class="row">
         <div class="col-xl-3 col-md-6">
             <div class="card bg-primary text-white mb-4">
-                <div class="card-body">Primary Card</div>
+                <div class="card-body">Total Users
+                    <?php
+                        $dash_users_query = "SELECT * FROM users";
+                        $dash_users_query_run = mysqli_query($con, $dash_users_query);
+
+                        if($user_total = mysqli_num_rows($dash_users_query_run))
+                        {
+                            echo '<h4 class="mb-0">' .$user_total.' </h4>';
+                        }
+                        else
+                        {
+                            echo '<h4 class="mb-0"> No data found. </h4>';
+                        }
+                    ?>
+                </div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
+                    <a class="small text-white stretched-link" href="view-register.php">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
         </div>
         <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Warning Card</div>
+                <div class="card-body">Total Patients
+                    <h4 class="mb-0">4</h4>
+                </div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-success text-white mb-4">
-                <div class="card-body">Success Card</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-danger text-white mb-4">
-                <div class="card-body">Danger Card</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="#">View Details</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
         </div>
     </div>
 </div>
